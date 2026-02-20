f31430f8a6237a7372cd6a40b2c962fceccb7ba6
John Cayley <John_Cayley@brown.edu>
2026-02-19 22:23:18 -0500

    with build

---
+++ dist/assets/notebookkit-B5ysaKSQ.js
@@ -0,0 +1,10 @@
+import{d as o}from"./index-BoQGnnIN.js";o({root:document.getElementById("cell-22"),expanded:[],variables:[]},{id:22,body:(e,n)=>{const t=e`### Note
+
+Even once you've done all the above and ironed out any bugs, you may still be surprised when you are trying to \`preview\` (by running the script and then hitting \`o+return\` to 'Open' your notebook as a site).
+
+You may get a **\`file not found\`** error because the site that Observable builds for you expects to find a notebook HTML file named \`index.html\` as the default entrance to the website it has just built. An immediate fix is to type at the end of whatever is in you browser's URL box, add the file name of your notebook: \`<whateverucalledit>.html\`. Alternatively, you could rename one of your notebook files as \`index.html\`.
+
+\`\`\`
+blockcode
+\`\`\`
+`;return n(t),{notecell:t}},inputs:["md","display"],outputs:["notecell"],output:void 0,assets:void 0,autodisplay:!1,autoview:void 0,automutable:void 0});o({root:document.getElementById("cell-23"),expanded:[],variables:[]},{id:23,body:e=>e.innerHTML,inputs:["notecell"],outputs:[],output:void 0,assets:void 0,autodisplay:!0,autoview:void 0,automutable:void 0});
